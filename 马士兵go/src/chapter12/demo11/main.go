54019c83086b3c3f8d3c1aa3b545a35ec726a4ba
chuang <zk-zhuangtb@chinaunicom.cn>
2025-11-25 22:53:48 +0800

    ok

--- 马士兵go/src/chapter12/demo11/main.go
+++ 马士兵go/src/chapter12/demo11/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	intChan := make(chan int, 1)
+	stringChan := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		intChan <- 10
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		stringChan <- "hello golang"
+	}()
+	select {
+	case v := <-intChan:
+		fmt.Println(v)
+	case v := <-stringChan:
+		fmt.Println(v)
+	default:
+		fmt.Println("防止select被阻塞")
+	}
 }
--- 马士兵go/src/chapter12/demo12/main.go
+++ 马士兵go/src/chapter12/demo12/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func printNum() {
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+	}
+}
+func devide() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("devide()出现错误", err)
+		}
+	}()
+	num1 := 10
+	num2 := 0
+	result := num1 / num2
+	fmt.Println(result)
+}
+
+func main() {
+	go printNum()
+	go devide()
+	time.Sleep(time.Second * 5)
 }
