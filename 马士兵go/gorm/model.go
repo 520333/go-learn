201c9092f6d73e46fe2ac4f6df40860a23872873
chuang <346299437@qq.com>
2025-12-01 21:05:05 +0800

    gorm模型-表名定义

--- 马士兵go/gorm/model.go
+++ 马士兵go/gorm/model.go
@@ -19,6 +19,9 @@ type Box struct {
 	gorm.Model
 }
 
+func (Box) TableName() string { //某张表特殊表名
+	return "my_box"
+}
 func Migrate() {
 	if err := DB.AutoMigrate(&Post{}, &Category{}, &PostCategory{}, &Box{}); err != nil {
 		log.Fatal(err)
