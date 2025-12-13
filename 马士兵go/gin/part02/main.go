f1085b96096ab271b5c66c8c0120711710393ea7
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-14 00:53:42 +0800

    get方式 路径中使用键值对参数

--- 马士兵go/gin/part02/main.go
+++ 马士兵go/gin/part02/main.go
@@ -11,6 +11,10 @@ func main() {
 	// 必须给定参数 /demo/123 否则404
 	r.GET("/demo/:id", myfunc.Hello1)
 	// *占位符 无参数直接忽略 输出:/
-	r.GET("/demo2/*id", myfunc.Hello1)
+	r.GET("/demo2/*id", myfunc.Hello2)
+
+	// 路径中以键值对传入参数 在路由规则中不用做文章
+	r.GET("/demo3", myfunc.Hello3)
+	r.GET("/demo4", myfunc.Hello4)
 	r.Run(":8080")
 }
--- 马士兵go/gin/part02/myfunc/user.go
+++ 马士兵go/gin/part02/myfunc/user.go
@@ -11,3 +11,23 @@ func Hello1(context *gin.Context) {
 	var id = context.Param("id")
 	context.String(http.StatusOK, "hello world %s", id)
 }
+
+func Hello2(context *gin.Context) {
+	// 获取路径中的参数值:
+	var id = context.Param("id")
+	context.String(http.StatusOK, "hello world %s", id)
+}
+
+func Hello3(context *gin.Context) {
+	// 获取路径中的参数值: 通过key获取对应value
+	var id = context.Query("id")
+	var name = context.Query("name")
+	context.String(http.StatusOK, "hello world %s %s", id, name)
+}
+
+func Hello4(context *gin.Context) {
+	// 获取路径中的参数值: 通过key获取对应value
+	var id = context.DefaultQuery("id", "123")
+	var name = context.DefaultQuery("name", "丽丽")
+	context.String(http.StatusOK, "hello world %s %s", id, name)
+}
