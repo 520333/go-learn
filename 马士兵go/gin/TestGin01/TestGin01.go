2ec0614f6276320fb8af2d8bc108c335e2d3027b
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-13 22:41:08 +0800

    静态文件引入

--- 马士兵go/gin/TestGin01/TestGin01.go
+++ 马士兵go/gin/TestGin01/TestGin01.go
@@ -15,6 +15,8 @@ func main() {
 
 	//r.LoadHTMLFiles("templates/hello01.html", "templates/hello02.html") // 不推荐
 	r.LoadHTMLGlob("templates/**/*") // 加载全部 多级两个星号
+	//r.Static("/s", "static") // 指定静态文件：指定css文件
+	r.StaticFS("/s", http.Dir("static"))
 
 	r.GET("/hello", Hello)
 	err := r.Run(":9999")
