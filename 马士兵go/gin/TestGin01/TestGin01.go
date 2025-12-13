06111ca095351649d67a8bd9bc7a17ab6068ced6
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-14 00:00:15 +0800

    数据交互-后端数据 前端渲染

--- 马士兵go/gin/TestGin01/TestGin01.go
+++ 马士兵go/gin/TestGin01/TestGin01.go
@@ -11,12 +11,12 @@ import (
 func main() {
 	r := gin.Default()
 
-	//r.LoadHTMLFiles("templates/hello01.html", "templates/hello02.html") // 不推荐
+	//r.LoadHTMLFiles("templates/hello01.html", "templates/map.html") // 不推荐
 	r.LoadHTMLGlob("templates/**/*") // 加载全部 多级两个星号
 	//r.Static("/s", "static") // 指定静态文件：指定css文件
 	r.StaticFS("/s", http.Dir("static"))
 
-	r.GET("/hello", myfunc.Hello3)
+	r.GET("/hello", myfunc.Hello7)
 	err := r.Run(":9999")
 	if err != nil {
 		log.Fatalln(err)
--- 马士兵go/gin/TestGin01/myfunc/dataubteraction.go
+++ 马士兵go/gin/TestGin01/myfunc/dataubteraction.go
@@ -28,3 +28,33 @@ func Hello3(context *gin.Context) {
 	var array = [3]int{10, 20, 30}
 	context.HTML(http.StatusOK, "demo01/hello01.html", array)
 }
+
+func Hello4(context *gin.Context) {
+	var arr []Student = []Student{
+		Student{"lili", 20},
+		Student{"dawn", 21},
+		Student{"yoda", 22},
+	}
+	context.HTML(http.StatusOK, "demo01/hello01.html", arr)
+}
+
+func Hello5(context *gin.Context) {
+	var a map[string]int = make(map[string]int, 3)
+	a["丽丽"] = 18
+	a["菲菲"] = 16
+	a["明明"] = 21
+	context.HTML(http.StatusOK, "demo01/map.html", a)
+}
+
+func Hello6(context *gin.Context) {
+	var a map[string]Student = make(map[string]Student, 3)
+	a["NO1"] = Student{"丽丽", 18}
+	a["NO2"] = Student{"菲菲", 21}
+	a["NO3"] = Student{"明明", 22}
+	context.HTML(http.StatusOK, "demo01/map-struct.html", a)
+}
+
+func Hello7(context *gin.Context) {
+	var slice = []int{1, 2, 3, 4, 5, 6}
+	context.HTML(http.StatusOK, "demo01/slice.html", slice)
+}
