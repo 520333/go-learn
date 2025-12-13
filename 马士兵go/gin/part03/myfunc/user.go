da5231442b435deff8166cd36bcf7d3bb4069ba0
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-14 01:35:05 +0800

    POST获取请求数据2

--- 马士兵go/gin/part03/myfunc/user.go
+++ 马士兵go/gin/part03/myfunc/user.go
@@ -13,5 +13,9 @@ func Hello1(context *gin.Context) {
 func Hello2(context *gin.Context) {
 	var uname = context.PostForm("username")
 	var pwd = context.PostForm("pwd")
-	fmt.Println(uname, pwd)
+	var age = context.DefaultPostForm("age", "18")
+	var loveLange = context.PostFormArray("loveLangue")
+	var user = context.PostFormMap("user")
+	context.String(http.StatusOK, "hello %s %s %s 兴趣爱好:%s  地址邮箱:%s", uname, pwd, age, loveLange, user)
+	fmt.Println(uname, pwd, age, loveLange, user)
 }
