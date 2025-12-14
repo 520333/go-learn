8fd5afde05eaac7c64df0a1c248095b149b977ac
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-14 23:51:59 +0800

    自定义中间件基本使用

--- 马士兵go/gin/part13/middleware/mw.go
+++ 马士兵go/gin/part13/middleware/mw.go
@@ -9,3 +9,9 @@ import (
 func MiddleWare01(context *gin.Context) {
 	fmt.Println("自定义中间件里面写统一的业务逻辑")
 }
+
+func MiddleWare02() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("这是自定义中间件-方式2")
+	}
+}
