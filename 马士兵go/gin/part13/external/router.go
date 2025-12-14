42a6fa6f55923e84d4eaef506da83243562ee7c5
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-15 00:09:32 +0800

    独立的路由组中间件、局部中间件使用

--- 马士兵go/gin/part13/external/router.go
+++ 马士兵go/gin/part13/external/router.go
@@ -1,9 +1,13 @@
 package external
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin/part13/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Router(r *gin.RouterGroup) {
-	r.GET("/userindex3", Hello4)
+	r.GET("/userindex3", Hello4, middleware.MiddleWare01)
 	r.POST("/toajax", Hello5)
 	r.GET("/userindex4/:uname/:age", Hello6)
 }
