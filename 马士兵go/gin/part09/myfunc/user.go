1d45b1e25823b90c5cc7164072706c2a29dce968
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-14 21:48:07 +0800

    模板with使用

--- 马士兵go/gin/part09/myfunc/user.go
+++ 马士兵go/gin/part09/myfunc/user.go
@@ -6,12 +6,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Student struct {
+	Name string
+	Age  int
+}
+
 func Hello1(context *gin.Context) {
 	var age = 19
 	var arr = []int{33, 66, 99}
+	flag := true
+	var username = "丽丽"
+
+	var stu = Student{
+		Name: "丽丽",
+		Age:  18,
+	}
 	var mapData = map[string]interface{}{
-		"age": age,
-		"arr": arr,
+		"age":      age,
+		"arr":      arr,
+		"flag":     flag,
+		"username": username,
+		"stu":      stu,
 	}
 	context.HTML(http.StatusOK, "demo01/hello.html", mapData)
 }
