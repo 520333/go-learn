9a8c7a39581e5fef0f7451cbe18691a5711847c4
chuang <346299437@qq.com>
2025-12-16 20:02:38 +0800

    日志封装

--- 马士兵go/gin-cms/README.md
+++ 马士兵go/gin-cms/README.md
@@ -3,5 +3,6 @@ go模块
 ```bash
 go get -u github.com/gin-gonic/gin
 go get -u github.com/spf13/viper
+go get -u github.com/sirupsen/logrus
 ```
 基于gin框架的内容管理系统后端接口案例
---
+++ 马士兵go/gin-cms/utils/log.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"io"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetLogger 设置日志
+func SetLogger() {
+	//1.设置集中的日志writer
+	// 创建文件 设置为writer
+	setLoggerWriter()
+
+	//2.配置日志信息
+	initLogger()
+}
+
+// 初始化日志
+func initLogger() {
+	// 使用json模式记录
+	slog.New(slog.NewJSONHandler(logWriter, &slog.HandlerOptions{}))
+}
+
+// logger
+var logger *slog.Logger
+
+func Logger() *slog.Logger {
+	return logger
+}
+
+// 设置writer
+func setLoggerWriter() {
+	//根据不同的mode选择不同的writer
+	switch gin.Mode() {
+	case gin.ReleaseMode:
+		// 创建文件
+		logfile := "./logs/app.log"
+		if file, err := os.OpenFile(logfile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666); err != nil {
+			log.Println(err)
+			return
+		} else {
+			logWriter = file
+		}
+
+	case gin.TestMode, gin.DebugMode:
+		fallthrough
+	default:
+		logWriter = os.Stdout
+	}
+}
+
+// 公共的writer变量
+var logWriter io.Writer
+
+func LogWriter() io.Writer {
+	return logWriter
+}
