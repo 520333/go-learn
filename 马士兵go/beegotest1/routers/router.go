ed2135039d824bf123ec6c220ce76a9a70729fd7
chuang <346299437@qq.com>
2025-12-20 16:10:01 +0800

    错误消息国际化

--- 马士兵go/beegotest1/routers/router.go
+++ 马士兵go/beegotest1/routers/router.go
@@ -13,6 +13,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/beego/beego/v2/core/validation"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -38,6 +39,7 @@ func init() {
 	test.Router("request-data/cookie", &TestRequestController{}, "post:Cookie")
 	test.Router("request-data/header", &TestRequestController{}, "get:Header")
 	test.Router("response-data", &TestRequestController{}, "get:Resp")
+	test.Router("valid-data", &TestRequestController{}, "get:Valid")
 	beego.AddNamespace(test)
 	//beego.Post("/test/post", func(ctx *context.Context) {
 	//	ctx.Input.Query("name")
@@ -57,6 +59,83 @@ type Article struct {
 	Subject   string `json:"subject,omitempty"`
 	Views     int    `json:"views,omitempty"`
 	Published bool   `json:"published,omitempty"`
+}
+type User struct {
+	Name       string   `valid:"Required;AlphaDash;MinSize(4);MaxSize(8)" label:"名称" json:"name"`
+	Topic      []string `valid:"MinSize(2)" label:"主题" json:"topic"`
+	Email      string   `valid:"Required;Email" label:"电子邮箱" json:"email"`
+	LoginIP    string   `valid:"IP;" label:"登录IP" json:"login_ip"`
+	ContactNum string   `valid:"Mobile;" label:"联系电话" json:"contact_num"`
+	Intro      string   `valid:"Match(/^Hello/)" label:"介绍" json:"intro"`
+}
+
+var MessageTmpls = map[string]string{
+	"Required":     "不能为空",
+	"Min":          "最小为 %d",
+	"Max":          "最大为 %d",
+	"Range":        "范围从 %d 到 %d",
+	"MinSize":      "最小长度为 %d",
+	"MaxSize":      "最大长度为 %d",
+	"Length":       "长度必须为 %d",
+	"Alpha":        "必须为字母字符",
+	"Numeric":      "必须为数字字符",
+	"AlphaNumeric": "必须为字母和数字字符",
+	"Match":        "必须匹配 %s",
+	"NoMatch":      "必须不能匹配 %s",
+	"AlphaDash":    "必须为字母和数字和(-_)字符",
+	"Email":        "必须为电子邮箱地址",
+	"IP":           "必须为IP地址",
+	"Base64":       "必须为Base64编码字符",
+	"Mobile":       "必须为手机号码",
+	"Tel":          "必须为固话号码",
+	"Phone":        "必须为手机或固话号码",
+	"ZipCode":      "必须为邮政编码",
+}
+
+func (c *TestRequestController) Valid() {
+	// 1.获取User类型数据 来自请求端
+	user := &User{}
+	// 2.执行验证
+	// 设置全局错误消息
+	validation.SetDefaultMessage(MessageTmpls)
+
+	validator := validation.Validation{}
+	validResult, err := validator.Valid(user)
+	if err != nil {
+		c.Data["json"] = map[string]any{
+			"code":    1,
+			"message": "数据验证规则错误,无法验证数据",
+		}
+		_ = c.ServeJSON()
+		return
+	}
+	// 3.获取验证结果,未通过获取错误消息
+	if !validResult {
+		errors := map[string][]string{}
+		for field, errs := range validator.ErrorsMap {
+			ms := []string{}
+			for _, err := range errs {
+				ms = append(ms, err.Message)
+			}
+			errors[field] = ms
+		}
+		// 获取错误消息
+		c.Data["json"] = map[string]any{
+			"code":    1,
+			"message": "数据验证失败",
+			//"errors":  validator.ErrorsMap,
+			"errors": errors,
+		}
+		_ = c.ServeJSON()
+		return
+	}
+
+	c.Data["json"] = map[string]any{
+		"code":    0,
+		"message": "数据验证通过",
+	}
+	_ = c.ServeJSON()
+	return
 }
 
 func (c *TestRequestController) Resp() {
