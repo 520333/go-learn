9ada554e1f5548fd64165baaa7bb89eda72ee7d9
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-21 19:51:40 +0800

    restful风格api通过mux三方包

---
+++ 马士兵go/microService/articleService.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func articleList(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "articleService: articleList")
+}
+func articleRetrieve(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "articleService: articleRetrieve")
+}
+func articleCreate(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "articleService: articleCreate")
+}
+func articleDelete(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "articleService: articleDelete")
+}
+func articleUpdate(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "articleService: articleUpdate")
+}
+func articleUpdatePartial(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "articleService: articleUpdatePartial")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	// rest api 定义
+	router.HandleFunc("/articles", articleList).Methods("GET")
+	router.HandleFunc("/articles/{id}", articleRetrieve).Methods("GET")
+	router.HandleFunc("/articles/", articleCreate).Methods("POST")
+	router.HandleFunc("/articles/{id}", articleDelete).Methods("DELETE")
+	router.HandleFunc("/articles/{id}", articleUpdate).Methods("PUT")
+	router.HandleFunc("/articles/{id}", articleUpdatePartial).Methods("PATCH")
+
+	// http监听
+	log.Fatalln(http.ListenAndServe(":8088", router))
+}
