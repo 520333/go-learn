41e6a5018263a0407d823ec00da4edceb706d5fb
chuang <zk-zhuangtb@chinaunicom.cn>
2025-12-21 23:04:45 +0800

    kong基于consul注册中心DNS服务发现的请求代理转发

--- 马士兵go/microService/productServerHealth.go
+++ 马士兵go/microService/productServerHealth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/hashicorp/consul/api"
@@ -20,7 +21,7 @@ func main() {
 	// 定义业务逻辑服务，假设为产品服务
 	server := http.NewServeMux()
 	server.HandleFunc("/info", func(writer http.ResponseWriter, request *http.Request) {
-		_, err := fmt.Fprintf(writer, "Product Service. %s", address)
+		_, err := fmt.Fprintf(writer, "Product Service. %s now is:%s", address, time.Now().String())
 		if err != nil {
 			log.Fatalln(err)
 		}
