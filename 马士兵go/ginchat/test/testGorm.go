92e07322e877b3359bc71df506839c6ca593a89a
chuang <346299437@qq.com>
2025-12-24 21:04:20 +0800

    gin准备及gorm引入

---
+++ 马士兵go/ginchat/test/testGorm.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"ginchat/models"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	var dsn = "root:123456@tcp(192.168.50.100:3306)/ginchat?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	//迁移
+	if err := db.AutoMigrate(&models.UserBasic{}); err != nil {
+		log.Println(err)
+	}
+
+	user := &models.UserBasic{}
+	user.Name = "海绵宝宝"
+	//user.PassWord = "123456"
+	db.Create(user)
+
+	// Read
+	fmt.Println(db.First(user, 1))
+	db.Model(user).Update("PassWord", "123456")
+}
