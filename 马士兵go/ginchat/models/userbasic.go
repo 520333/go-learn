957bb1efb4eebd2b0378dcd2b9ed1fce5f5ccfd8
阿宝 <dawn@qq.com>
2025-12-26 20:18:37 +0800

    md5工具类及注册密码加密

--- 马士兵go/ginchat/models/userbasic.go
+++ 马士兵go/ginchat/models/userbasic.go
@@ -17,6 +17,7 @@ type UserBasic struct {
 	Identity      string
 	ClientIp      string
 	ClientPort    string
+	Salt          string
 	LoginTime     *time.Time
 	HeartbeatTime *time.Time
 	LoginOutTime  *time.Time `gorm:"column:login_out_time" json:"login_out_time"`
@@ -35,6 +36,22 @@ func GetUserList() []*UserBasic {
 		fmt.Println(v)
 	}
 	return data
+}
+
+func FindUserByName(name string) UserBasic {
+	user := UserBasic{}
+	utils.DB.Where("name = ?", name).First(&user)
+	return user
+}
+
+func FindUserByPhone(phone string) *gorm.DB {
+	user := UserBasic{}
+	return utils.DB.Where("phone = ?", phone).First(&user)
+}
+
+func FindUserByEmail(email string) *gorm.DB {
+	user := UserBasic{}
+	return utils.DB.Where("email = ?", email).First(&user)
 }
 
 func CreateUser(user UserBasic) *gorm.DB {
