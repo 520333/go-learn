1b2fe110c172e3f22e7f582f08bf48dbc87708e2
阿宝 <dawn@qq.com>
2026-02-26 15:43:20 +0800

    宝哥代驾微服务-验证码模块验证码生成实现

--- 马士兵go/bigDJ/backend/verifyCode/internal/service/veritycode.go
+++ 马士兵go/bigDJ/backend/verifyCode/internal/service/veritycode.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"math/rand"
 
 	pb "verifyCode/api/verityCode"
 )
@@ -15,5 +16,34 @@ func NewVerityCodeService() *VerityCodeService {
 }
 
 func (s *VerityCodeService) GetVerityCode(ctx context.Context, req *pb.GetVerityCodeRequest) (*pb.GetVerityCodeReply, error) {
-	return &pb.GetVerityCodeReply{}, nil
+	return &pb.GetVerityCodeReply{
+		Code: RandCode(int(req.Length), req.Type),
+	}, nil
+}
+
+func RandCode(l int, t pb.TYPE) string {
+	switch t {
+	case pb.TYPE_DEFAULT:
+		fallthrough
+	case pb.TYPE_DIGIT:
+		return randCode("0123456789", l)
+	case pb.TYPE_LETTER:
+		return randCode("abcdefghijklmnopqrstuvwxyz", l)
+	case pb.TYPE_MIXED:
+		return randCode("0123456789abcdefghijklmnopqrstuvwxyz", l)
+	default:
+	}
+	return ""
+}
+
+func randCode(chars string, l int) string {
+	charsLen := len(chars)
+
+	result := make([]byte, l)
+	for i := 0; i < l; i++ {
+		randIndex := rand.Intn(charsLen)
+		result[i] = chars[randIndex]
+	}
+
+	return string(result)
 }
